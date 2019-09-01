e2fc18e9032d06904469b159dd7512d1b718a4ea
Víctor Rosa <vicrosa25@gmail.com>
2019-09-01 14:17:01 +0200

    Lecture 1

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -1,17 +1,14 @@
 package lectures;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Person;
-
 import com.google.common.collect.ImmutableList;
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import com.google.common.collect.Lists;
 import mockdata.MockData;
-import org.assertj.core.util.Lists;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Lecture1 {
@@ -19,8 +16,35 @@ public class Lecture1 {
   @Test
   public void imperativeApproach() throws IOException {
     List<Person> people = MockData.getPeople();
+
     // 1. Find people aged less or equal 18
+    List<Person> youngPeople = new ArrayList<>();
+    List<Person> young10 = Lists.newArrayList();
+
+    for(Person person : people) {
+      if (person.getAge() <= 18)
+        youngPeople.add(person);
+    }
+
+    for(Person person : youngPeople) {
+      System.out.println(person.getAge());
+    }
+
+
     // 2. Then change implementation to find first 10 people
+    System.out.println();
+    System.out.println("Only the first 10th people");
+
+    for (int i = 0; young10.size() <= 9; i++) {
+      if(people.get(i).getAge() <= 18) {
+        young10.add(people.get(i));
+      }
+    }
+
+    for (Person person : young10) {
+      System.out.println(person.getAge());
+    }
+
 
   }
 
